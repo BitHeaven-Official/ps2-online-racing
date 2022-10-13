798127798a1ba2295f7cdd7dd47a88b97103f1a3
BitHeaven <bit.heaven@bk.ru>
2022-10-13 18:49:55 +0500

    using tabs

--- gs.c
+++ gs.c
@@ -1,32 +1,42 @@
+static qword_t *gs_cmd_buffer;
+
+
+struct draw_state {
+	framebuffer_t fb;
+	zbuffer_t zb;
+};
+
+
 int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
 {
-  framebuffer_t fb;
-  fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
-  fb.width = width;
-  fb.height = height;
-  fb.psm = psm;
-  fb.mask = 0;
+  
+	framebuffer_t fb;
+	fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
+	fb.width = width;
+	fb.height = height;
+	fb.psm = psm;
+	fb.mask = 0;
 
-  z->address = graph_vram_allocate(width, height, psmz, GRAPH_ALIGN_PAGE);
-  z->enable = 0;
-  z->method = 0;
-  z->zsm = 0;
-  z->mask = 0;
+	z->address = graph_vram_allocate(width, height, psmz, GRAPH_ALIGN_PAGE);
+	z->enable = 0;
+	z->method = 0;
+	z->zsm = 0;
+	z->mask = 0;
 
-  graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE); 
-  graph_set_screen(0, 0, width, height);
-  graph_set_bgcolor(0, 0, 0);
-  graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
-  graph_enable_output();
+	graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE); 
+	graph_set_screen(0, 0, width, height);
+	graph_set_bgcolor(0, 0, 0);
+	graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
+	graph_enable_output();
 
-  qword_t *q = buf;
-  memset(buf, 0, DRAWBUF_LEN);
-  q = draw_setup_environment(q, 0, &fb, z);
-  q = draw_primitive_xyoffset(q, 0, 2048-(VID_W/2), 2048-(VID_H/2));
-  q = draw_finish(q);
-  dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-  draw_wait_finish();
+	qword_t *q = buf;
+	memset(buf, 0, DRAWBUF_LEN);
+	q = draw_setup_environment(q, 0, &fb, z);
+	q = draw_primitive_xyoffset(q, 0, 2048-(VID_W/2), 2048-(VID_H/2));
+	q = draw_finish(q);
+	dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
+	draw_wait_finish();
 
-  return 0;
+	return 0;
 }
 
--- main.c
+++ main.c
@@ -30,85 +30,85 @@ zbuffer_t *z;
 
 
 static int tri[] = {
-  10, 10, 0,
-  500, 20, 0,
-  300, 400, 0
+	10, 10, 0,
+	500, 20, 0,
+	300, 400, 0
 };
 
 qword_t *draw(qword_t *q)
 {
-  uint64_t red = 0xf0;
-  uint64_t blue = 0x0f;
-  uint64_t green = 0x0f;
+	uint64_t red = 0xf0;
+	uint64_t blue = 0x0f;
+	uint64_t green = 0x0f;
 
-  // SET PRIM
-  q->dw[0] = 0x1000000000000001;
-  q->dw[1] = 0x000000000000000e;
-  q++;
-  q->dw[0] = GS_SET_PRIM(GS_PRIM_TRIANGLE, 0, 0, 0, 0, 0, 0, 0, 0);
-  q->dw[1] = GS_REG_PRIM;
-  q++;
-  // 6 regs, x1, EOP
-  q->dw[0] = 0x6000000000008001;
-  // GIFTag header - col, pos, col, pos, col, pos
-  q->dw[1] = 0x0000000000515151;
-  q++;
+	// SET PRIM
+	q->dw[0] = 0x1000000000000001;
+	q->dw[1] = 0x000000000000000e;
+	q++;
+	q->dw[0] = GS_SET_PRIM(GS_PRIM_TRIANGLE, 0, 0, 0, 0, 0, 0, 0, 0);
+	q->dw[1] = GS_REG_PRIM;
+	q++;
+	// 6 regs, x1, EOP
+	q->dw[0] = 0x6000000000008001;
+	// GIFTag header - col, pos, col, pos, col, pos
+	q->dw[1] = 0x0000000000515151;
+	q++;
 
-  int cx = BITftoi4(2048 - (VID_W/2));
-  int cy = BITftoi4(2048 - (VID_H/2));
+	int cx = BITftoi4(2048 - (VID_W/2));
+	int cy = BITftoi4(2048 - (VID_H/2));
 
-  for(int i = 0; i < 3; i++) {
-    q->dw[0] = (red&0xff) | (green&0xff)<<32;
-    q->dw[1] = (blue&0xff) | (SHIFT_AS_I64(0x80, 32));
-    q++;
+	for(int i = 0; i < 3; i++) {
+		q->dw[0] = (red&0xff) | (green&0xff)<<32;
+		q->dw[1] = (blue&0xff) | (SHIFT_AS_I64(0x80, 32));
+		q++;
     
-    int base = i*3;
-    int x = BITftoi4(tri[base+0]) + cx;
-    int y = BITftoi4(tri[base+1]) + cy;
-    int z = 0;
-    q->dw[0] = x | SHIFT_AS_I64(y, 32);
-    q->dw[1] = z; 
-    q++;
-  }
+		int base = i*3;
+		int x = BITftoi4(tri[base+0]) + cx;
+		int y = BITftoi4(tri[base+1]) + cy;
+		int z = 0;
+		q->dw[0] = x | SHIFT_AS_I64(y, 32);
+		q->dw[1] = z; 
+		q++;
+	}
 
-  return q;
+	return q;
 }
 
 int main()
 {
-  printf("Hello\n");
-  buf = malloc(DRAWBUF_LEN);
-  z = malloc(sizeof(zbuffer_t));
-  // init DMAC
-  dma_channel_initialize(DMA_CHANNEL_GIF, 0, 0);
-  dma_channel_fast_waits(DMA_CHANNEL_GIF);
-  // initialize graphics mode
-  // 640x480 (4:3)
-  // 853x480 (16:9)
-  // 768x576 (4:3)
-  // 1024x576 (16:9)
-  int vmode = graph_get_region();
-  gs_init(VID_W, VID_H, GS_PSM_32, GS_PSMZ_32, vmode, GRAPH_MODE_INTERLACED);
+	printf("Hello\n");
+	buf = malloc(DRAWBUF_LEN);
+	z = malloc(sizeof(zbuffer_t));
+	// init DMAC
+	dma_channel_initialize(DMA_CHANNEL_GIF, 0, 0);
+	dma_channel_fast_waits(DMA_CHANNEL_GIF);
+	// initialize graphics mode
+	// 640x480 (4:3)
+	// 853x480 (16:9)
+	// 768x576 (4:3)
+	// 1024x576 (16:9)
+	int vmode = graph_get_region();
+	gs_init(VID_W, VID_H, GS_PSM_32, GS_PSMZ_32, vmode, GRAPH_MODE_INTERLACED);
 
-  graph_wait_vsync();
+	graph_wait_vsync();
 
-  while(1) {
-    dma_wait_fast();
-    qword_t *q = buf;
-    memset(buf, 0, DRAWBUF_LEN);
-    // clear
-    q = draw_disable_tests(q, 0, z);
-    q = draw_clear(q, 0, 2048.0f - 320, 2048.0f - 244, VID_W, VID_H, 10, 10, 10);
-    q = draw_enable_tests(q, 0, z);
-    q = draw(q);
-    q = draw_finish(q);
-    dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-    // print_buffer(buf, q-buf);
+	while(1) {
+		dma_wait_fast();
+		qword_t *q = buf;
+		memset(buf, 0, DRAWBUF_LEN);
+		// clear
+		q = draw_disable_tests(q, 0, z);
+		q = draw_clear(q, 0, 2048.0f - 320, 2048.0f - 244, VID_W, VID_H, 10, 10, 10);
+		q = draw_enable_tests(q, 0, z);
+		q = draw(q);
+		q = draw_finish(q);
+		dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
+		// print_buffer(buf, q-buf);
 
-    draw_wait_finish();
+		draw_wait_finish();
 
-    // wait vsync
-    graph_wait_vsync();
-    // sleep(2);
-  }
+		// wait vsync
+		graph_wait_vsync();
+		// sleep(2);
+	}
 }

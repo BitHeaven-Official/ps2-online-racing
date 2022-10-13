3ed0acc14ebfb3f4ead32cb8ff878c0c3c6a6408
BitHeaven <bit.heaven@bk.ru>
2022-10-13 18:44:04 +0500

    Move code to files

--- main.c
+++ main.c
@@ -29,60 +29,6 @@ static qword_t *buf;
 zbuffer_t *z;
 
 
-int print_buffer(qword_t *b, int len)
-{
-  printf("-- buffer\n");
-  for(int i = 0; i < len; i++) {
-    printf("%016llx %016llx\n", b->dw[0], b->dw[1]);
-    b++;
-  }
-  printf("-- /buffer\n");
-
-  return 0;
-}
-
-int gs_finish()
-{
-  qword_t *q = buf;
-  q = draw_finish(q);
-  dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-  dma_wait_fast();
-
-  return 0;
-}
-
-int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
-{
-  framebuffer_t fb;
-  fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
-  fb.width = width;
-  fb.height = height;
-  fb.psm = psm;
-  fb.mask = 0;
-
-  z->address = graph_vram_allocate(width, height, psmz, GRAPH_ALIGN_PAGE);
-  z->enable = 0;
-  z->method = 0;
-  z->zsm = 0;
-  z->mask = 0;
-
-  graph_set_mode(gmode, vmode, GRAPH_MODE_FIELD, GRAPH_DISABLE); 
-  graph_set_screen(0, 0, width, height);
-  graph_set_bgcolor(0, 0, 0);
-  graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
-  graph_enable_output();
-
-  qword_t *q = buf;
-  memset(buf, 0, DRAWBUF_LEN);
-  q = draw_setup_environment(q, 0, &fb, z);
-  q = draw_primitive_xyoffset(q, 0, 2048-(VID_W/2), 2048-(VID_H/2));
-  q = draw_finish(q);
-  dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-  draw_wait_finish();
-
-  return 0;
-}
-
 static int tri[] = {
   10, 10, 0,
   500, 20, 0,
@@ -157,7 +103,7 @@ int main()
     q = draw(q);
     q = draw_finish(q);
     dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
-    print_buffer(buf, q-buf);
+    // print_buffer(buf, q-buf);
 
     draw_wait_finish();
 

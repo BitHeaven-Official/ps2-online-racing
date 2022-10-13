9292c104848a6f4522ef4ffc6c5bbf16b3bdcff8
BitHeaven <bit.heaven@bk.ru>
2022-10-13 18:54:23 +0500

    change variable

--- gs.c
+++ gs.c
@@ -1,3 +1,5 @@
+#define GS_CMD_BUFFER_LEN (40 * 16)
+
 static qword_t *gs_cmd_buffer;
 
 
@@ -9,7 +11,9 @@ struct draw_state {
 
 int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
 {
-  
+	if(!gs_cmd_buffer) {
+		gs_cmd_buffer = malloc(GS_CMD_BUFFER_LEN);
+	}
 	framebuffer_t fb;
 	fb.address = graph_vram_allocate(width, height, psm, GRAPH_ALIGN_PAGE);
 	fb.width = width;
@@ -29,12 +33,12 @@ int gs_init(int width, int height, int psm, int psmz, int vmode, int gmode)
 	graph_set_framebuffer_filtered(fb.address, width, psm, 0, 0);
 	graph_enable_output();
 
-	qword_t *q = buf;
-	memset(buf, 0, DRAWBUF_LEN);
+	qword_t *q = gs_cmd_buffer;
+	memset(gs_cmd_buffer, 0, DRAWBUF_LEN);
 	q = draw_setup_environment(q, 0, &fb, z);
 	q = draw_primitive_xyoffset(q, 0, 2048-(VID_W/2), 2048-(VID_H/2));
 	q = draw_finish(q);
-	dma_channel_send_normal(DMA_CHANNEL_GIF, buf, q-buf, 0, 0);
+	dma_channel_send_normal(DMA_CHANNEL_GIF, gs_cmd_buffer, q-gs_cmd_buffer, 0, 0);
 	draw_wait_finish();
 
 	return 0;
